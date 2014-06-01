92053e1611ec508b3abd6482f6425c872e93dd1c
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2014-06-01 20:00:51 +0200

    Changed example to show all volumes instead of current R directory

--- inst/example/server.R
+++ inst/example/server.R
@@ -2,6 +2,6 @@ library(shiny)
 library(shinyFiles)
 
 shinyServer(function(input, output) {
-    output$file <- shinyFileChoose(input, 'file', root=R.home(), restrictions=system.file(package='base'))
-    output$filepaths <- renderPrint({parseFilePaths(R.home(), input$file)})
+    output$file <- shinyFileChoose(input, 'file', roots=getVolumes(), restrictions=system.file(package='base'))
+    output$filepaths <- renderPrint({parseFilePaths(getVolumes()(), input$file)})
 })
